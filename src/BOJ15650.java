fc83c16e5a1172acb732ec116e24f1033f68a326
daegyu-song <sdk0109@naver.com>
2024-03-21 21:40:07 +0900

    BOJ15650

---
+++ src/BOJ15650.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ15650 {
+    static int N, M;
+    static int[] selected;
+
+    static StringBuilder sb = new StringBuilder();
+
+    static void solution(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(" ");
+            sb.append("\n");
+        } else {
+            int start = selected[k - 1] + 1;
+            if (start == 0) start = 1;
+            for (int i = start; i <= N; i++) {
+                selected[k] = i;
+                solution(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M + 1];
+
+        solution(1);
+        System.out.println(sb.toString());
+    }
+}
