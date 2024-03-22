ba4c5316bb73bc0f1c6e03117f065c9256a77644
daegyu-song <sdk0109@naver.com>
2024-03-22 22:08:11 +0900

    AS14888

---
+++ src/AS14888.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+public class AS14888 {
+    static int N;
+    static int[] numbers;
+    static int[] operators = new int[4];
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+    static void solution(int answer, int k) {
+        if (k == N) {
+            if (answer > max) max = answer;
+            if (answer < min) min = answer;
+        } else {
+            for (int i = 0; i < operators.length; i++) {
+                if (operators[i] == 0) continue;
+                operators[i]--;
+                switch (i) {
+                    case 0:
+                        solution(answer + numbers[k], k + 1);
+                        break;
+                    case 1:
+                        solution(answer - numbers[k], k + 1);
+                        break;
+                    case 2:
+                        solution(answer * numbers[k], k + 1);
+                        break;
+                    case 3:
+                        solution(answer / numbers[k], k + 1);
+                        break;
+                }
+                operators[i]++;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < operators.length; i++) {
+            operators[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(numbers[0], 1);
+        System.out.println(max + "\n" + min);
+    }
+}
