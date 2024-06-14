e536566b5f9695ea8dcb26aa29624970b978d350
daegyu-song <sdk0109@naver.com>
2024-06-15 00:46:10 +0900

    BOJ11052

--- src/dp/BOJ11052.java
+++ src/dp/BOJ11052.java
@@ -17,15 +17,10 @@ public class BOJ11052 {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        dy[1] = numbers[1];
-        dy[2] = Math.max(dy[1] + dy[1], numbers[2]);
-
-        for (int i = 3; i <= n; i++) {
-            int max = Integer.MIN_VALUE;
-            for (int j = 1; j <= i / 2; j++) {
-                max = Math.max(max, dy[j] + dy[i - j]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dy[i] = Math.max(dy[i], dy[i - j] + numbers[j]);
             }
-            dy[i] = Math.max(max, numbers[i]);
         }
 
         System.out.println(dy[n]);
